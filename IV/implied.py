95949966c84c70a457750842b92ac6561a5ac4d4
Emil Robert Melconian <emelconi@uwo.ca>
2025-10-03 09:42:50 -0400

    starting implied.py

---
+++ IV/implied.py
@@ -0,0 +1,10 @@
+# this file will calculate implied moves 
+
+import pandas as pd
+import yfinance as yf
+import math
+
+def currentImpliedMove(symbol, earningsDate):
+    t = yf.Ticker(symbol)
+    expiries = 
+    
